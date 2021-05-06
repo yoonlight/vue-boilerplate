dad1ea6dbd19905d62bae23572be882969a727d3
kjw <yoonlight@gmail.com>
2021-05-07 02:13:57 +0900

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    children: [
+      { path: '', component: () => import('../components/HelloWorld.vue') }
+    ]
+  },
+  { path: '/about', component: () => import('../views/About.vue') },
+  { path: '/register', component: () => import('../views/Register.vue') },
+  { path: '/login', component: () => import('../views/Login.vue') }
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <app-bar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppBar from "../components/layout/AppBar.vue";
+
+export default {
+  name: "Home",
+
+  components: {
+    AppBar,
+  },
+};
+</script>
